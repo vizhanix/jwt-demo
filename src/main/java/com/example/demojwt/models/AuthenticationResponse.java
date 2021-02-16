63948e29d4d773a1172c1b91fbb59838e2ae074a
mixon <mixontandukar@gmail.com>
2021-02-16 19:46:49 +0545

    model and util added

---
+++ src/main/java/com/example/demojwt/models/AuthenticationResponse.java
@@ -0,0 +1,18 @@
+package com.example.demojwt.models;
+
+public class AuthenticationResponse {
+	
+	private final String jwt;
+
+	public AuthenticationResponse(String jwt) {
+		super();
+		this.jwt = jwt;
+	}
+
+	public String getJwt() {
+		return jwt;
+	}
+	
+	
+
+}
